ab2d48865d9a642898c29847b31392569cecc9d3
Thiago Marcelo Uchôa Marques <thiago.uchoa18@gmail.com>
2021-08-31 23:00:12 -0400

    adição do desafio de fazer um site que modifica de acordo com as horas do dia

---
+++ Javascript/desafios/01 - desafio das horas/script.js
@@ -0,0 +1,34 @@
+function carregar(){
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    if(hora < 6){
+        img.src='dia02.png';
+        document.body.style.backgroundColor = "#778AA8"
+        msg.innerHTML = "Boa Madrugada!<br>"
+    }else if(hora < 12){
+        img.src='dia01.png';
+        document.body.style.backgroundImage = "linear-gradient(to right,#566D8C,#969DAD,#CACACA)"
+        msg.innerHTML = "Bom Dia!<br>"
+    }else if(hora<18){
+        img.src="tarde.png";
+        document.body.style.backgroundImage = "linear-gradient(to right,#353B41,#6D5F53,#7D624F)"
+        msg.innerHTML = "Boa Tarde!<br>"
+    }else{
+        img.src='noite.png';
+        document.body.style.backgroundImage = "linear-gradient(to right,#7A5670,#5D5273,#484E72)"
+        msg.innerHTML = "Boa Noite!<br>"
+    }
+    if(hora != 1){
+        msg.innerHTML += `Agora são ${hora} horas e ${minutos}`     
+    }else{
+        msg.innerHTML += `Agora são ${hora} hora e ${minutos}`     
+    }
+    if(minutos != 1){
+        msg.innerHTML += " minutos"
+    }else{
+        msg.innerHTML += " minuto"
+    }
+}
